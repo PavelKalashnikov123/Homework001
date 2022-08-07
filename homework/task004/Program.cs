6517a3616222005e62da9ebb56d6080b7852b418
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2022-08-07 14:51:45 +0300

    task 4

---
+++ homework/task004/Program.cs
@@ -0,0 +1,21 @@
+﻿int i = 1;
+bool n = true;
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + number);
+while (i <= number)
+{
+ if (i % 2 != 1)
+ {
+    Console.Write(i + ", ");
+    n = false;
+ }
+ i++;
+}
+
+if (n)
+{
+ Console.WriteLine("Нет чётных чисел!");
+}
