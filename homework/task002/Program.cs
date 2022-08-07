7042a2d75a87ba15195060e072bb1215bbe5203d
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2022-08-07 14:36:16 +0300

    task 2

---
+++ homework/task002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 3 числа:");
+int number0 = Convert.ToInt32(Console.ReadLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number0;
+
+if (number1 > max)
+{
+    max = number1;
+}
+
+if (number2 > max)
+{
+max = number2;
+}
+
+Console.WriteLine("Наибольшее из введённых чисел " + max);
