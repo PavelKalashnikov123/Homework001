48588322fed9b93afde8206555e36de7e8854bd6
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2022-08-07 14:27:36 +0300

    task 1

---
+++ homework/task001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+ Console.WriteLine("Первое число " + number1 + " больше чем второе " + number2);
+}
+else
+{
+ Console.WriteLine("Второе число " + number2 + " больше чем первое " + number1);
+}
+
